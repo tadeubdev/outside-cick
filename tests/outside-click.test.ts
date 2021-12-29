f076ebaf701154365ab34efe8c2f5e133e3db2c4
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-12-29 16:15:56 -0300

    feat: ensure OutsideClick can create an element

--- tests/outside-click.test.ts
+++ tests/outside-click.test.ts
@@ -1,5 +1,56 @@
+/**
+ * @jest-environment jsdom
+ */
+
+class OutsideClick {
+    private element: Element|null = null;
+
+    constructor(element: Element|string) {
+        this.setElement(element);
+    }
+
+    private setElement(element: string | Element) {
+        const domElement = typeof element === 'string' ? document.querySelector(element) : element;
+        if (!domElement) {
+            throw new Error('Error when trying to set element');
+        }
+        this.element = domElement;
+    }
+
+    getElement(): Element|null {
+        return this.element;
+    }
+}
+
 describe('Outside click', () => {
-    test('initial test', () => {
-        expect(true).toBe(true);
+    beforeEach(() => {
+        document.body.innerHTML = '';
+    });
+
+    test('ensure it can receive a HTML Element in contructor', () => {
+        const elmPanel = document.createElement('div');
+
+        const outsideClick = new OutsideClick(elmPanel);
+
+        expect(outsideClick).toBeInstanceOf(OutsideClick);
+    });
+
+    test('ensure it can receive a string on contructor', () => {
+        window.document.body.appendChild(document.createElement('div'));
+        const elmName = 'div';
+
+        const outsideClick = new OutsideClick(elmName);
+
+        expect(outsideClick).toBeInstanceOf(OutsideClick);
+    });
+
+    test('ensure it can transform the string on HTML Element on constructor', () => {
+        window.document.body.appendChild(document.createElement('div'));
+        const elmName = 'div';
+
+        const outsideClick = new OutsideClick(elmName);
+        const element = outsideClick.getElement();
+
+        expect(element).toBeInstanceOf(HTMLDivElement);
     });
 });
