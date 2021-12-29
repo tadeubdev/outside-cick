9da9bce331b4fc39cd690160d85a9f90e7b07f5e
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-12-29 10:48:28 -0300

    feat: ensure elements clicked in and out works

--- src/index.js
+++ src/index.js
@@ -1,5 +1,67 @@
-const appName = 'Webpack Demo App';
+let started = false;
+const items = [];
 
-setTimeout(
-  () => alert(`Thanks for using ${appName}`)
-, 500);
+function generateElementFromString(element) {
+    return document.querySelector(element);
+}
+
+function getParentsElements(element, parents) {
+    parents = parents || [element];
+    const parent = element.parentElement;
+    if (parent) {
+        parents.push(parent);
+        return getParentsElements(parent, parents);
+    }
+    return parents;
+}
+
+function handleClickOndocument(event) {
+    const parentsElements = getParentsElements(event.target);
+    const nonClickedItems = items.filter(item => {
+        if (item.clickOutMethod === undefined) {
+            return false;
+        }
+        if (item.elmAction && parentsElements.includes(item.elmAction)) {
+            return false;
+        }
+        return parentsElements.includes(item.element) === false;
+    });
+    nonClickedItems.forEach(item => item.clickOutMethod());
+    const clickedItems = items.filter(item => {
+        if (item.clickInMethod === undefined) {
+            return false;
+        }
+        if (item.elmAction && parentsElements.includes(item.elmAction)) {
+            return true;
+        }
+        return parentsElements.includes(item.element);
+    });
+    clickedItems.forEach(item => item.clickInMethod());
+}
+
+function registerClickOnDocument() {
+    document.addEventListener('click', (event) => handleClickOndocument(event));
+}
+
+export default function () {
+    if (!started) {
+        started = true;
+        registerClickOnDocument();
+    }
+    return {
+        add(element, clickOutMethod = null, clickInMethod = null) {
+            let finalElement = null;
+            if (typeof element === 'string') {
+                finalElement = generateElementFromString(element);
+            } else if (element instanceof Element || element instanceof Document) {
+                finalElement = element;
+            }
+            items.push({
+                element: finalElement,
+                clickOutMethod: clickOutMethod,
+                clickInMethod: clickInMethod,
+            });
+        },
+    };
+};
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,24 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: './outside-click.js'
+        filename: './outside-click.js',
+        libraryTarget: 'window',
+        libraryExport: 'default',
+        library: 'OutsideClick',
     },
     watch: true,
     module: {
-    rules: [
-    {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
-      }]
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
     }
 };
